dfec8c7b4f316482611558092e84ce4dcbc41443
francisvtran <115505020+francisvtran@users.noreply.github.com>
2025-01-27 21:38:07 -0600

    Hid Keys
    -hid Django Secret Key
    -added Python Decouple

--- novoweatherapp/settings.py
+++ novoweatherapp/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-70c%9jog-07%d6(842+u0$z*dl5*^z4vzxrn-9g-9c36@)colj'
+SECRET_KEY = config('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
