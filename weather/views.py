a256cbe6ac0c518e6de3e47fd91e276d2055896f
francisvtran <115505020+francisvtran@users.noreply.github.com>
2025-01-28 00:51:50 -0600

    Added sample cities to database
    -Added Denver, Austin, Bethesda, and Houston sample cities to database
    -Added loop to get weather for each city and add it to a list
    -Passed the list to template in for loop

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.conf import settings
+from .models import City
 import requests
 
 def index(request):
@@ -10,17 +11,25 @@ def index(request):
     if not appid:
         raise ValueError("OpenWeatherMap API key is not set. Check your settings.")
 
+    cities = City.objects.all()
+
     zip_code = '20810,us'
 
-    city_weather = requests.get(url.format(zip_code) + f"&appid={appid}").json()
+    weather_data = []
 
-    weather = {
-        'city' : city_weather['name'],
-        'temp_min' : city_weather['main']['temp_min'],
-        'temp_max' : city_weather['main']['temp_max'],
-        'icon' : city_weather['weather'][0]['icon']
-    }
+    for city in cities:
 
-    context = {'weather' : weather}
+        city_weather = requests.get(url.format(zip_code) + f"&appid={appid}").json()
+
+        weather = {
+            'city' : city_weather['name'],
+            'temp_min' : city_weather['main']['temp_min'],
+            'temp_max' : city_weather['main']['temp_max'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)
+
+    context = {'weather_data' : weather_data}
 
     return render(request, 'weather/index.html', context)
