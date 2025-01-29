61ef97a89e7fc1ff96cad2207d6c5ecc025b4e19
francisvtran <115505020+francisvtran@users.noreply.github.com>
2025-01-28 20:43:26 -0600

    Created Form Unit Tests
    -Created 6 unit tests to verify form functionality
    -Testing for valid zip code
    -Testing for non-numeric zip code (symbol and alphabetical)
    -Testing for zip codes that are too long or short
    -Testing for duplicated zip code

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .forms import CityForm
 
-# Create your tests here.
+#Form Unit Tests
+class CityFormTest(TestCase):
+    def test_valid_zip_code(self):
+        form = CityForm(data={'zip_code': '20810'})
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_zip_code_non_numeric(self):
+        form = CityForm(data={'zip_code': 'ABCDE'})  
+        self.assertFalse(form.is_valid())
+        self.assertIn('zip_code', form.errors)
+        self.assertEqual(form.errors['zip_code'][0], "Enter a valid 5-digit ZIP Code.")
+
+    def test_invalid_zip_code_non_numeric_symbols(self):
+        form = CityForm(data={'zip_code': '!@#$%'})  
+        self.assertFalse(form.is_valid())
+        self.assertIn('zip_code', form.errors)
+        self.assertEqual(form.errors['zip_code'][0], "Enter a valid 5-digit ZIP Code.")
+
+    def test_invalid_zip_code_short(self):
+        form = CityForm(data={'zip_code': '123'})  
+        self.assertFalse(form.is_valid())
+        self.assertIn('zip_code', form.errors)
+        self.assertEqual(form.errors['zip_code'][0], "Enter a valid 5-digit ZIP Code.")
+
+    def test_invalid_zip_code_long(self):
+        form = CityForm(data={'zip_code': '123'})  
+        self.assertFalse(form.is_valid())
+        self.assertIn('zip_code', form.errors)
+        self.assertEqual(form.errors['zip_code'][0], "Enter a valid 5-digit ZIP Code.")
+
+    def test_duplicate_zip_code(self):
+        # First, save a valid ZIP code to the database
+        form = CityForm(data={'zip_code': '20810'})
+        if form.is_valid():
+            form.save()
+        # Attempt to save the same ZIP code again
+        form = CityForm(data={'zip_code': '20810'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('zip_code', form.errors)
